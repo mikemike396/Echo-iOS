93df7b8bf019415ea8aa5b8ff432a426b0615e2b
Michael Kushinski <mikekushinski@gmail.com>
2023-10-26 14:19:39 -0400

    Reorganizes Feed cell and adds feed icon

--- Packages/Features/Feed/Sources/FeedScreen.swift
+++ Packages/Features/Feed/Sources/FeedScreen.swift
@@ -49,6 +49,7 @@ public struct FeedScreen: View {
                     }
 
                 }
+                .listRowSeparator(.visible)
             }
             .listStyle(.plain)
         }
@@ -56,27 +57,32 @@ public struct FeedScreen: View {
     }
 
     private func cell(item: RSSFeedItem) -> some View {
-        HStack(alignment: .top, spacing: 0) {
-            WebImage(url: item.imageURL)
-                .resizable()
-                .scaledToFill()
-                .frame(width: 100, height: 100)
-                .clipped()
-                .cornerRadius(20)
-                .padding(.trailing, 12)
-
-            Text(item.title ?? "")
-                .font(.body)
-                .foregroundStyle(item.hasRead ? .secondary : .primary)
-                .frame(maxWidth: .infinity, alignment: .leading)
-                .multilineTextAlignment(.leading)
-
-            Text(formattedPublishedDate(item.publishedDate) ?? "")
-                .font(.footnote)
-                .frame(alignment: .trailing)
-                .lineLimit(1)
-                .foregroundStyle(.secondary)
-                .padding(.leading, 4)
+        VStack(spacing: 0) {
+            HStack(alignment: .top, spacing: 12) {
+                WebImage(url: item.imageURL)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .clipped()
+                    .cornerRadius(20)
+                Text(item.title ?? "")
+                    .font(.body)
+                    .foregroundStyle(item.hasRead ? .secondary : .primary)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .multilineTextAlignment(.leading)
+            }
+            HStack(spacing: 5) {
+                Spacer()
+                WebImage(url: item.feed?.imageURL)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 15, height: 15)
+                Text(formattedPublishedDate(item.publishedDate) ?? "")
+                    .font(.footnote)
+                    .frame(alignment: .trailing)
+                    .lineLimit(1)
+                    .foregroundStyle(.secondary)
+            }
         }
     }
 
